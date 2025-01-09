406f387fe0843bb1872fcd538abb40efa24b24b2
robertotics4 <robertooliveiragp@gmail.com>
2025-01-09 15:49:37 -0300

    feat: initial config aws

---
+++ src/main/java/com/lrostech/challenge/aws_file_manager/AwsFileManagerApplication.java
@@ -0,0 +1,13 @@
+package com.lrostech.challenge.aws_file_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsFileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsFileManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lrostech/challenge/aws_file_manager/AwsFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrostech.challenge.aws_file_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsFileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
